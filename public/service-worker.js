024b36af595e9ac05c34c1a93ef0c09d672f45e8
lironkerem <lironkerem@gmail.com>
2026-04-20 14:06:17 +0300

    Dark Mode CSS Patch

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 // The Curiosity Path — Service Worker
 
-const CACHE_VERSION  = 'tcp-2026-04-19-1644';
+const CACHE_VERSION  = 'tcp-2026-04-20-1404';
 const CACHE_NAME     = `tcp-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE  = `tcp-runtime-${CACHE_VERSION}`;
 const ICON_PATH      = './Icons/';
